fa1ee79dc666f37daac9f39b430e36c594444d43
Katie Lamber02 <katielamber02@gmail.com>
2020-09-09 20:14:37 +0300

    token generated and sent as part of link v5

--- controllers/auth.js
+++ controllers/auth.js
@@ -21,50 +21,67 @@ const ses = new AWS.SES({ apiVersion: '2010-12-01' });
 exports.register = (req, res) => {
   // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/ses-examples-sending-email.html
   const { name, email, password } = req.body;
-  const params = {
-    Source: process.env.EMAIL_FROM,
-    Destination: {
-      ToAddresses: [email],
-    },
-    ReplyToAddresses: [process.env.EMAIL_TO],
-    Message: {
-      /* required */
-      Body: {
+
+  User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      return res.status(400).json({
+        error: 'Email has aleady been taken',
+      });
+    }
+    const token = jwt.sign(
+      { name, email, password },
+      process.env.JWT_ACCOUNT_ACTIVATION,
+      {
+        expiresIn: '10m',
+      }
+    );
+    console.log('TOKEN1:', token);
+
+    const params = {
+      Source: process.env.EMAIL_FROM,
+      Destination: {
+        ToAddresses: [email],
+      },
+      ReplyToAddresses: [process.env.EMAIL_TO],
+      Message: {
         /* required */
-        Html: {
-          Charset: 'UTF-8',
-          Data: `<html><h1>Hello, ${name}
+        Body: {
+          /* required */
+          Html: {
+            Charset: 'UTF-8',
+            Data: `<html><h1>Hello, ${name}
                 Verify your email with the following link:
                 
                 </h1 style="color:blue">
                 <p>${process.env.CLIENT_URL}/auth/activate/${token}</p></html>`,
+          },
+          Text: {
+            Charset: 'UTF-8',
+            Data: 'TEXT_FORMAT_BODY',
+          },
         },
-        Text: {
+        Subject: {
           Charset: 'UTF-8',
-          Data: 'TEXT_FORMAT_BODY',
+          Data: 'Complete registration',
         },
       },
-      Subject: {
-        Charset: 'UTF-8',
-        Data: 'Complete registration',
-      },
-    },
-  };
-  const sendRegistrationEmail = ses.sendEmail(params).promise();
-  sendRegistrationEmail
-    .then((data) => {
-      console.log('data submitted to SES:', data);
-      res.json({
-        message: `Email has been sent to ${email}`,
-        // {message: "Email has been sent to katielamber02@gmail.com"}
+    };
+    const sendRegistrationEmail = ses.sendEmail(params).promise();
+    sendRegistrationEmail
+      .then((data) => {
+        console.log('data submitted to SES:', data);
+        res.json({
+          message: `Email has been sent to ${email}`,
+          // {message: "Email has been sent to katielamber02@gmail.com"}
+        });
+      })
+      .catch((error) => {
+        console.log('error when data submitted to SES:', error);
+        res.json({
+          message: `Email could not be verified`,
+        });
+        // {error: "Email could not be verified"}
+        //  message: `Email could not be verified`
       });
-    })
-    .catch((error) => {
-      console.log('error when data submitted to SES:', error);
-      res.json({
-        message: `Email could not be verified`,
-      });
-      // {error: "Email could not be verified"}
-      //  message: `Email could not be verified`
-    });
+  });
 };
